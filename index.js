a0bb52337234dcb645bee633006e71b666f42436
Umeshchaurasiya123 <umeshc1998@gmail.com>
2025-08-14 02:11:59 +0530

    Add initial project files with dotenv and OpenAI integration

---
+++ index.js
@@ -0,0 +1,59 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import OpenAI from "openai";
+const client = new OpenAI({
+  apiKey: process.env.APIKEYGEMMINI,
+  baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/",
+});
+
+const response = await client.chat.completions.create({
+  model: "gemini-2.0-flash",
+  messages: [
+    {
+      role: "system",
+      content: `
+                You're an AI assistant expert in coding with Javascript. You only and only know Javascript as coding language.
+                If user asks anything other than Javascript coding question, Do not ans that question.
+                You are an AI from ChaiCode which is an EdTech company transforming modern tech knowledge. Your name is ChaiCode and always ans as if you represent ChaiCode
+
+                Examples:
+                Q: Hey There
+                A: Hey, Nice to meet you. How can I help you today? Do you want me to show what we are cooking at ChaiCode.
+
+                Q: Hey, I want to learn Javascript
+                A: Sure, Why don't you visit our website ot YouTube at chaicode for more info.
+
+                Q: I am bored
+                A: What about a JS Quiz?
+
+                Q: Can you write a code in Python?
+                A: I can, but I am designed to help in JS
+            `,
+    },
+    { role: "user", content: "Hey gpt, My name is Piyush Garg" },
+    {
+      role: "assistant",
+      content: "Hello Piyush Garg! How can I assist you today?",
+    },
+    { role: "user", content: "What is my name?" },
+    {
+      role: "assistant",
+      content: "Your name is Piyush Garg. How can I help you further?",
+    },
+    {
+      role: "user",
+      content: "Hey, what is my name again?",
+    },
+    {
+      role: "assistant",
+      content: "Your name is Piyush Garg. How can I assist you today?",
+    },
+    {
+      role: "user",
+      content: "lets write factorial code in and javascript",
+    },
+  ],
+});
+
+console.log(response.choices[0].message.content);
