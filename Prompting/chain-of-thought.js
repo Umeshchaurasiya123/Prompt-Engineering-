eb4fe6ee6a8d95f11dde5b413c46261cf97d6d98
Umeshchaurasiya123 <umeshc1998@gmail.com>
2025-08-18 23:38:23 +0530

    Implement chain of thought process with OpenAI integration and dotenv configuration

---
+++ Prompting/chain-of-thought.js
@@ -0,0 +1,116 @@
+import dotenv from "dotenv";
+dotenv.config({
+  path: "../.env", // Adjust path as needed
+});
+
+import OpenAI from "openai";
+const client = new OpenAI({
+  apiKey: process.env.APIKEYGEMMINI,
+  baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/",
+});
+
+async function chain_of_thought() {
+  const SYSTEM_PROMPT = `
+    You are an AI assistant who works on START, THINK and OUTPUT format.
+    For a given user query first think and breakdown the problem into sub problems.
+    You should always keep thinking and thinking before giving the actual output.
+    Also, before outputing the final result to user you must check once if everything is correct.
+
+    Rules:
+    - Strictly follow the output JSON format
+    - Always follow the output in sequence that is START, THINK, EVALUATE and OUTPUT.
+    - After evey think, there is going to be an EVALUATE step that is performed manually by someone and you need to wait for it.
+    - Always perform only one step at a time and wait for other step.
+    - Alway make sure to do multiple steps of thinking before giving out output.
+
+    Output JSON Format:
+    { "step": "START | THINK | EVALUATE | OUTPUT", "content": "string" }
+
+    Example:
+    User: Can you solve 3 + 4 * 10 - 4 * 3
+    ASSISTANT: { "step": "START", "content": "The user wants me to solve 3 + 4 * 10 - 4 * 3 maths problem" } 
+    ASSISTANT: { "step": "THINK", "content": "This is typical math problem where we use BODMAS formula for calculation" } 
+    ASSISTANT: { "step": "EVALUATE", "content": "Alright, Going good" } 
+    ASSISTANT: { "step": "THINK", "content": "Lets breakdown the problem step by step" } 
+    ASSISTANT: { "step": "EVALUATE", "content": "Alright, Going good" } 
+    ASSISTANT: { "step": "THINK", "content": "As per bodmas, first lets solve all multiplications and divisions" }
+    ASSISTANT: { "step": "EVALUATE", "content": "Alright, Going good" }  
+    ASSISTANT: { "step": "THINK", "content": "So, first we need to solve 4 * 10 that is 40" } 
+    ASSISTANT: { "step": "EVALUATE", "content": "Alright, Going good" } 
+    ASSISTANT: { "step": "THINK", "content": "Great, now the equation looks like 3 + 40 - 4 * 3" }
+    ASSISTANT: { "step": "EVALUATE", "content": "Alright, Going good" } 
+    ASSISTANT: { "step": "THINK", "content": "Now, I can see one more multiplication to be done that is 4 * 3 = 12" } 
+    ASSISTANT: { "step": "EVALUATE", "content": "Alright, Going good" } 
+    ASSISTANT: { "step": "THINK", "content": "Great, now the equation looks like 3 + 40 - 12" } 
+    ASSISTANT: { "step": "EVALUATE", "content": "Alright, Going good" } 
+    ASSISTANT: { "step": "THINK", "content": "As we have done all multiplications lets do the add and subtract" } 
+    ASSISTANT: { "step": "EVALUATE", "content": "Alright, Going good" } 
+    ASSISTANT: { "step": "THINK", "content": "so, 3 + 40 = 43" } 
+    ASSISTANT: { "step": "EVALUATE", "content": "Alright, Going good" } 
+    ASSISTANT: { "step": "THINK", "content": "new equations look like 43 - 12 which is 31" } 
+    ASSISTANT: { "step": "EVALUATE", "content": "Alright, Going good" } 
+    ASSISTANT: { "step": "THINK", "content": "great, all steps are done and final result is 31" }
+    ASSISTANT: { "step": "EVALUATE", "content": "Alright, Going good" }  
+    ASSISTANT: { "step": "OUTPUT", "content": "3 + 4 * 10 - 4 * 3 = 31" } 
+  `;
+
+  const messages = [
+    {
+      role: "system",
+      content: SYSTEM_PROMPT,
+    },
+    { role: "user", content: "Hey can you solve 4*12-12+12*1/3+4" },
+  ];
+
+  // Start the chain of thought process
+
+  while (true) {
+    const response = await client.chat.completions.create({
+      model: "gemini-2.0-flash",
+      messages: messages,
+      response_format: { type: "json_object" },
+    });
+
+    const content = response.choices[0].message.content;
+    const parsedContent = JSON.parse(content);
+    // console.log(parsedContent);
+    if (parsedContent.step == "START") {
+      console.log(`start-->>`, parsedContent.content);
+      messages.push({
+        role: "assistant",
+        content: JSON.stringify(parsedContent),
+      });
+      continue;
+    }
+
+    if (parsedContent.step == "THINK") {
+      console.log(`think-->`, parsedContent.content);
+      messages.push({
+        role: "assistant",
+        content: JSON.stringify(parsedContent),
+      });
+      continue;
+    }
+    if (parsedContent.step == "EVALUATE") {
+      messages.push({
+        role: "assistant",
+        content: JSON.stringify(parsedContent),
+      });
+      console.log(`evaluate-->`, parsedContent.content);
+      // Wait for manual evaluation
+      // For now, we will just continue to the next step
+      continue;
+    }
+
+    if (parsedContent.step == "OUTPUT") {
+      messages.push({
+        role: "assistant",
+        content: JSON.stringify(parsedContent),
+      });
+      console.log(`output-->`, parsedContent.content);
+      break;
+    }
+  }
+}
+
+chain_of_thought();
